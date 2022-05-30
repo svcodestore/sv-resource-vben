9d6ce056d69608195b093dd24aa144b9b1372dad
yangwb1123 <1832227346@qq.com>
2022-05-30 15:17:10 +0800

    feat(page): add pages

--- src/locales/lang/en/routes/menu.ts
+++ src/locales/lang/en/routes/menu.ts
@@ -3,4 +3,10 @@ export default {
   Dashboard: 'Dashboard',
   Analysis: 'Analysis',
   Workbench: 'Workbench',
+  FileManager: 'File Manager',
+  MyFile: 'My File',
+  MyFiles: 'File List',
+  Task: 'Tasks',
+  TaskUpload: 'Upload Tasks',
+  TaskDownload: 'Download Tasks',
 };
--- src/locales/lang/zh-CN/routes/menu.ts
+++ src/locales/lang/zh-CN/routes/menu.ts
@@ -3,4 +3,10 @@ export default {
   Dashboard: '仪表盘',
   Analysis: '分析页',
   Workbench: '工作台',
+  FileManager: '文件管理',
+  MyFile: '我的文件',
+  MyFiles: '文件列表',
+  Task: '任务列表',
+  TaskUpload: '上传任务列表',
+  TaskDownload: '下载任务列表',
 };
